60fed77d29e415785dfcaadbbcf33d3e8524ab79
AustinesSmith <austinemacarayo@gmail.com>
2025-01-19 14:59:42 +0800

    Create pending.js

---
+++ scripts/cmds/pending.js
@@ -0,0 +1,91 @@
+module.exports = {
+  config: {
+    name: "pending",
+    version: "1.0",
+    aliases: ["pend"],
+    author: "لوفي",
+    countDown: 5,
+    role: 2,
+    shortDescription: {
+      vi: "",
+      en: ""
+    },
+    longDescription: {
+      vi: "",
+      en: ""
+    },
+    category: "box chat"
+  },
+
+  langs: {
+    en: {
+      invaildNumber: "%1 is not a valid number",
+      cancelSuccess: "Refused %1 thread!",
+      approveSuccess: "Approved successfully %1 threads!",
+      cantGetPendingList: "Can't get the pending list!",
+      returnListPending: "»「PENDING」«❮ The whole number of threads to approve is: %1 thread ❯\n\n%2",
+      returnListClean: "「PENDING」There is no thread in the pending list"
+    }
+  },
+
+  onReply: async function({ api, event, Reply, getLang, commandName, prefix }) {
+    if (String(event.senderID) !== String(Reply.author)) return;
+    const { body, threadID, messageID } = event;
+    let count = 0;
+
+    if (isNaN(body) && (body.indexOf("c") == 0 || body.indexOf("cancel") == 0)) {
+      const index = body.slice(1).split(/\s+/);
+      for (const singleIndex of index) {
+        if (isNaN(singleIndex) || singleIndex <= 0 || singleIndex > Reply.pending.length) {
+          return api.sendMessage(getLang("invaildNumber", singleIndex), threadID, messageID);
+        }
+        api.removeUserFromGroup(api.getCurrentUserID(), Reply.pending[singleIndex - 1].threadID);
+        count += 1;
+      }
+      return api.sendMessage(getLang("cancelSuccess", count), threadID, messageID);
+    } else {
+      const index = body.split(/\s+/);
+      for (const singleIndex of index) {
+        if (isNaN(singleIndex) || singleIndex <= 0 || singleIndex > Reply.pending.length) {
+          return api.sendMessage(getLang("invaildNumber", singleIndex), threadID, messageID);
+        }
+        api.sendMessage(
+          "「 𝗔𝗣𝗣𝗥𝗢𝗩𝗘𝗗 」\n\n•This thread is officially approved by the admin.\nEnjoy using the bot and please do not spam. ♡\n— [ も.kaizenji ]",
+          Reply.pending[singleIndex - 1].threadID
+        );
+        count += 1;
+      }
+      return api.sendMessage(getLang("approveSuccess", count), threadID, messageID);
+    }
+  },
+
+  onStart: async function({ api, event, getLang, commandName }) {
+    const { threadID, messageID } = event;
+    let msg = "", index = 1;
+
+    try {
+      const spam = await api.getThreadList(100, null, ["OTHER"]) || [];
+      const pending = await api.getThreadList(100, null, ["PENDING"]) || [];
+      const list = [...spam, ...pending].filter(group => group.isSubscribed && group.isGroup);
+
+      for (const single of list) {
+        msg += `${index++}/ ${single.name}(${single.threadID})\n`;
+      }
+
+      if (list.length != 0) {
+        return api.sendMessage(getLang("returnListPending", list.length, msg), threadID, (err, info) => {
+          global.GoatBot.onReply.set(info.messageID, {
+            commandName,
+            messageID: info.messageID,
+            author: event.senderID,
+            pending: list
+          });
+        }, messageID);
+      } else {
+        return api.sendMessage(getLang("returnListClean"), threadID, messageID);
+      }
+    } catch (e) {
+      return api.sendMessage(getLang("cantGetPendingList"), threadID, messageID);
+    }
+  }
+};
