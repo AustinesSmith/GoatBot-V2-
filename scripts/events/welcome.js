58844acb5cdff1119396d6a3a6d619c056effbf2
AustinesSmith <austinemacarayo@gmail.com>
2025-01-25 08:40:56 +0800

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -26,7 +26,8 @@ module.exports = {
 			session2: "noon",
 			session3: "afternoon",
 			session4: "evening",
-			welcomeMessage: "Thank you for inviting me to the group!\nBot prefix: %1\nTo view the list of commands, please enter: %1help type .support to be added in support gc",
+			welcomeMessage: "▂▃▄▅▆▇█▓▒░ ᴡᴀʀʟᴏʀᴅ ░▒▓█▇▆▅▄▃▂ 
+	Thank you for inviting me to the group!\nBot prefix: %1\nTo view the list of commands, please enter: %1help type .support to be added in support gc",
 			multiple1: "you",
 			multiple2: "you guys",
 			defaultWelcomeMessage: `Hello {userName}.\nWelcome {multiple} to the chat group: {boxName}\nHave a nice {session} 😊 type .support to be added in support gc`
